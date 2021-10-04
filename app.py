2f48af917be6b16de66949d91cb9bb47ca289c59
rvibek <rvibek@gmail.com>
2021-10-04 20:30:28 +0000

    webapp

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ import plotly.express as px
 st.set_page_config( 
     page_title= 'Sales Dashboard',
     page_icon= ':bar_chart:',
-    
+    layout='wide'
 )
 
 
-df = pd.read_excel( 
+@st.cache
+def get_data_from_excel():
+    df = pd.read_excel( 
         'https://github.com/Sven-Bo/streamlit-sales-dashboard/blob/main/supermarkt_sales.xlsx?raw=true',
         sheet_name='Sales',
         engine='openpyxl',
@@ -21,6 +23,119 @@ df = pd.read_excel(
         usecols='B:R'
 
 )
+    # Add 'hour' column to dataframe
+    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    return df
+
+df = get_data_from_excel()
+
+
+
+#-----
+st.sidebar.header("Filter")
+
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
+
+customer_type = st.sidebar.multiselect(
+    "Select the Customer Type:",
+    options=df["Customer_type"].unique(),
+    default=df["Customer_type"].unique()
+)
+
+gender = st.sidebar.multiselect(
+    "Select the Gender:",
+    options=df["Gender"].unique(),
+    default=df["Gender"].unique()
+)
+
+
+df_selection = df.query(
+    "City == @city & Customer_type == @customer_type & Gender == @gender"
+
+)
+
+# st.dataframe(df_selection)
+
+
+
+#---
 st.title('My first app')
 st.write("Dapibus eget dictum magnis tristique orci odio leo suspendisse, netus natoque turpis accumsan sapien egestas iaculis. Porttitor curae mattis euismod senectus ante varius nullam natoque malesuada dictum, venenatis fringilla quam ultrices tempus sapien feugiat parturient et, imperdiet dictumst sagittis cubilia vehicula leo aptent dis velit. Taciti eget est tincidunt malesuada condimentum nunc aenean netus, posuere ad at felis leo ullamcorper sociis nostra tellus, lobortis etiam fringilla penatibus inceptos imperdiet iaculis.")
 
+total_sales = int(df_selection["Total"].sum())
+average_rating = round(df_selection["Rating"].mean(), 1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sale_by_transaction = round(df_selection['Total'].mean(), 2)
+
+st.markdown("---")
+
+left_column, middle_column, right_column = st.columns(3)
+
+with left_column:
+    st.subheader("Total Sales")
+    st.subheader(f"US ${total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales per Transaction:")
+    st.subheader(f"US ${average_sale_by_transaction}")
+
+st.markdown("---")
+
+
+# SALES BY PRODUCT LINE [BAR CHART]
+sales_by_product_line = (
+    df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+)
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x="Total",
+    y=sales_by_product_line.index,
+    orientation="h",
+    title="<b>Sales by Product Line</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
+    template="plotly_white",
+)
+fig_product_sales.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
+
+
+# SALES BY HOUR [BAR CHART]
+sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
+fig_hourly_sales = px.bar(
+    sales_by_hour,
+    x=sales_by_hour.index,
+    y="Total",
+    title="<b>Sales by hour</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
+    template="plotly_white",
+)
+fig_hourly_sales.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False)),
+)
+
+
+left_column, right_column = st.columns(2)
+left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
+right_column.plotly_chart(fig_product_sales, use_container_width=True)
+
+
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
